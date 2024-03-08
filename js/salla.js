44592a4ac78070647c319e6d507097370d1968bf
toqa-alaa <toqahabiba261@gmail.com>
2024-03-08 15:12:53 +0200

    second commit

--- js/salla.js
+++ js/salla.js
@@ -27,12 +27,25 @@ function toggleLanguage() {
 
   if (currentPage.includes('indexRTL.html')) {
     // If already in RTL page, switch to LTR
-    newPage = './index.html';
+    newPage = '../index.html';
   } else {
     // If in any other page or 'index.html', switch to RTL
-    newPage = './indexRTL.html';
+    newPage = '../indexRTL.html';
   }
 
   // Redirect to the appropriate page
   window.location.href = newPage;
 }
+document.addEventListener("DOMContentLoaded", function () {
+  var dropDown = document.querySelector('.drop-down');
+  var dropMenu = document.querySelector('.drop-menu');
+
+  dropDown.addEventListener('click', function () {
+      // Toggle the visibility of the drop-menu
+      if (dropMenu.style.display === 'none' || dropMenu.style.display === '') {
+          dropMenu.style.display = 'block';
+      } else {
+          dropMenu.style.display = 'none';
+      }
+  });
+});
